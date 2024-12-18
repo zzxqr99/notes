f7b346aba3c9f0f77d4564fde9765ec912199abe
zimeng大美女ZZz <134780502+zzxqr99@users.noreply.github.com>
2024-12-18 21:54:17 +0800

    Create leetcode11.java

---
+++ algorithm/leetcode11.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = maxArea(height);
+        System.out.println("最大盛水面积为: " + result);
+    }
+}
